f284a494bc7a27e2f07829297da8b1205633a188
kumibriones21 <kumibrionesschool@gmail.com>
2024-12-09 12:06:09 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataBindingDemoComponent } from "./data-binding-demo/data-binding-demo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DataBindingDemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/data-binding-demo/data-binding-demo.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding-demo',
+  standalone: true,
+  imports: [ FormsModule ],
+  templateUrl: './data-binding-demo.component.html',
+  styleUrl: './data-binding-demo.component.css'
+})
+export class DataBindingDemoComponent {
+  message="My First App!"
+  description="This is my new Angular Application"
+  imageUrl = '../images/angular.jpg';
+  w = 100;
+  h = 100;
+  altText = "Angular Logo";
+  textColor='skyblue';
+  ishighlighted = true;
+  yourName='';
+
+  count =0;
+  increment() {
+    this.count++;
+  }
+  decrement() {
+    this.count--;
+  }
+}
